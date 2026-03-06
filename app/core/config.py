37b6a0b28b377a36d799889108a55d0a75946164
mvxyoffcial-create <mvxyoffcial@gmail.com>
2026-03-06 15:50:05 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -5,19 +5,19 @@ from typing import Optional
 class Settings(BaseSettings):
     APP_NAME: str = "TouristApp"
     APP_ENV: str = "development"
-    SECRET_KEY: str = "change-this-secret-key-in-production-must-be-32-chars"
+    SECRET_KEY: str = "venuboy267"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     REFRESH_TOKEN_EXPIRE_DAYS: int = 30
 
-    MONGODB_URL: str = "mongodb://localhost:27017"
+    MONGODB_URL: str = "mongodb+srv://Natravels:zero8907@cluster0.x8s6sie.mongodb.net/?appName=Cluster0"
     DATABASE_NAME: str = "tourist_db"
 
     SMTP_HOST: str = "smtp.gmail.com"
     SMTP_PORT: int = 587
-    SMTP_USER: str = ""
-    SMTP_PASSWORD: str = ""
-    EMAIL_FROM: str = ""
+    SMTP_USER: str = "natravelsoffcail@gmail.com"
+    SMTP_PASSWORD: str = "qpha qkbn rytr ncvu"
+    EMAIL_FROM: str = "natravelsoffcail@gmail.com"
     EMAIL_FROM_NAME: str = "TouristApp"
 
     GOOGLE_CLIENT_ID: str = ""
